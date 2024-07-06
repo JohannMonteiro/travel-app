62112c8960786b591b234d293dfa96dc18e93092
Johann Monteiro <monteiro.johann@gmail.com>
2024-07-06 13:24:06 -0300

    finished Guide section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -16,7 +16,7 @@ const CampSite = ({
 }: CampProps) => {
   return (
     <div
-      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl xl:rounded-5xl`}
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 ">
         <div className="flexCenter gap-4">
@@ -76,7 +76,7 @@ const Camp = () => {
       {/* Popup */}
       <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
         <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
-          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+          <h2 className="regular-24 md:regular-32 xl:regular-64 capitalize text-white">
             <strong>Feeling Lost</strong> and Not Knowing The Way?
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
